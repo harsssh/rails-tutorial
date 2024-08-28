4fe9834983a33860d1e792ff219265d5d75b33e2
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-08-28 10:52:46 +0900

    文字列の繰り返しを避ける

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,25 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Rails Tutorial"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Rails Tutorial"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | Rails Tutorial"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | Rails Tutorial"
+    assert_select "title", "About | #{@base_title}"
   end
 end
